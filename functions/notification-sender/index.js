12fb651e7448ee4e83f734f939f7f36b6c702999
Jhony Rivero <jhony.rivero@hotmail.com>
2021-05-30 14:45:17 -0500

    state machine ready to deploy

--- functions/notification-sender/index.js
+++ functions/notification-sender/index.js
@@ -1,19 +1,47 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({ region: process.env.REGION });
+
+const sns = new AWS.SNS({ apiVersion: '2010-03-31', region: process.env.REGION});
+
+function validateInput(event) {
+  return event && event.subject && event.subject.length > 0 && event.message && event.message.length > 0; 
 }
 
+async function sendNotificationToSns(message, subject) {
+  const params = {
+    Message: message,
+    Subject: subject,
+    TopicArn: process.env.SNS_TOPIC
+  };
+  return sns.publish(params).promise();
+}
+
+
 /**
- * Sample Lambda function which mocks the operation of checking the current price of a stock.
- * For demonstration purposes this Lambda function simply returns a random integer between 0 and 100 as the stock price.
+ * Sample Lambda function which mocks the operation of selling a random number of shares for a stock.
+ * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
  * 
  * @param {Object} event - Input event to the Lambda function
  * @param {Object} context - Lambda Context runtime methods and attributes
  *
- * @returns {Object} object - Object containing the current price of the stock
+ * @returns {Object} object - Object containing details of the stock selling transaction
  * 
  */
 exports.handler = async (event, context) => {
-    // Check current price of the stock
-    stock_price = getRandomInt(100)  // Current stock price is mocked as a random integer between 0 and 100
-    return { 'stock_price': stock_price }
+  try {
+    if (!validateInput(event)) {
+      throw new Error('Bad Request, message and subject are required.');
+    }
+    const response = await sendNotificationToSns(event.message, event.subject);
+    console.log('publish response:', response);
+    return {
+      statusCode: 202,
+      message: event.message
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
